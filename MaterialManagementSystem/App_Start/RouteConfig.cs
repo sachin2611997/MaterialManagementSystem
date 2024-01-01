b51b8cc7aaeaa56d6e9a31bfb67dfd376d03b777
User <User@DESKTOP-V5ECKUA>
2024-01-01 16:36:14 +0530

    publish in local server

--- MaterialManagementSystem/App_Start/RouteConfig.cs
+++ MaterialManagementSystem/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MaterialManagementSystem
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "IndentList", id = UrlParameter.Optional }
             );
         }
     }
